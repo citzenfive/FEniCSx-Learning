659e2cdc10f953580f0225d705826a3ee52d520f
João Víctor de Oliveira <joaovictoroliveira1452@gmail.com>
2026-07-13 15:15:19 -0300

    feat: just a minor file with some options

--- get_types.py
+++ get_types.py
@@ -2,6 +2,6 @@ from mpi4py import MPI
 from petsc4py import PETSc
 
 opts = PETSc.Options()
-opts["help"] = None
+opts["help"] = None  # type: ignore[attr-defined]
 ksp = PETSc.KSP().create()
 ksp.setFromOptions()
